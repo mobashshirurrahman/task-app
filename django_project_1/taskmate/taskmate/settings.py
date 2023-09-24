0247b779c77edb9b5ab2f083f7571ea7693501fc
Mobashshirur Rahman <mobashshirurrahman@gmail.com>
2023-09-24 20:30:00 +0530

    Railway updates

--- django_project_1/taskmate/taskmate/settings.py
+++ django_project_1/taskmate/taskmate/settings.py
@@ -15,7 +15,7 @@ import environ
 
 env = environ.Env()
 environ.Env.read_env()
- 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
@@ -28,8 +28,8 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
-
-ALLOWED_HOSTS = []
+# "localhost", "127.0.0.1", "taskmate.up.railway.app"
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -126,10 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     (os.path.join(BASE_DIR, 'static'))
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
