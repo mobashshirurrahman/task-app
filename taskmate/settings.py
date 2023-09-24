55d7c14c84a75247e8f3a84e8b5ce1d0a677488e
Mobashshirur Rahman <mobashshirurrahman@gmail.com>
2023-09-24 21:56:55 +0530

    host update

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = env('DJANGO_DEBUG')
 # "localhost", "127.0.0.1", "taskmate.up.railway.app"
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "task-app-production-786.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ['task-app-production-786.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [' https://task-app-production-786.up.railway.app']
 
 # Application definition
 
